3f1ed8ab35690c9a996bfdc5268fdc2d6ea9e2dc
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-17 17:13:06 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,3 +16,15 @@ def select_winner(passengers)
   end
   winner
 end
+
+#birthday_kids = {
+#"Timmy" => 9,
+#"Sarah" => 6,
+#"Amanda" => 27
+#}
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
