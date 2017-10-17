b449a4e8e1dd2588c126be646a1497ed3f87a497
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-17 17:07:19 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,12 +9,12 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  passengers.each do |suit, name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
   end
-
+  winner
 end
 
-winner
+
